e31aa321c7bb8e1f0cfd51a5af72b86d6e0f76a2
Ben Hadwen <hadwenb@frenchconnection.com>
2013-08-21 18:19:58 +0100

    in progress

--- js/backbone-test.js
+++ js/backbone-test.js
@@ -1,8 +1,23 @@
 var StaffMember = Backbone.Model.extend({});
 
-// var CalendarView = Backbone.View.extend({model:Calendar});
+
 
 var staffMember = new StaffMember(
 	{ id:1, firstName: 'Ben', lastName: 'Hadwen', user_entitlement:20 }
 	);
+
+var StaffMemberView = Backbone.View.extend({
+	render: function(){
+		var html = '<p>' + this.model.get('firstName') + '</p>' + '<p>' + this.model.get('lastname') + '</p>';
+		$(this.el).html(html);
+	}
+});
+
+var staffMemberView = new StaffMemberView({model: staffMember});
+
+staffMemberView.render();
+
+
+
+
 
