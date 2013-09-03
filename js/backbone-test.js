5252ff0c979a94ed6c18b37fd07375449cfc9f6e
Ben Hadwen <hadwenb@frenchconnection.com>
2013-09-03 17:37:37 +0100

    working version

--- js/backbone-test.js
+++ js/backbone-test.js
@@ -3,49 +3,84 @@
 
 // MODEL
 var StaffMember = Backbone.Model.extend({
-	// defaults: function(){
-	// 	return({
-	// 	firstName: "firstName",
-	// 	lastName: "lastName",
-	// 	entitlement: 0
-	// 	});
-	// },
+	defaults: function(){
+		return({
+		firstName: "firstName",
+		lastName: "lastName",
+		entitlement: 0
+		});
+	},
 	// urlRoot : '/hadwensites/holidayplanner/user.js'
 	// urlRoot : '/hadwensites/holidayplanner/user.js'
 	//urlRoot: "/hadwensites/holidayplanner/user.json"
-	urlRoot: "/hadwensites/holidayplanner/js/data/usertemp.json"
+	urlRoot: "/hadwensites/holidayplanner/js/data/usertemp.json",
+
+	toggleStatus: function(){
+		console.log("toggleStatus");
+		if(this.get('status') === 'incomplete'){
+			this.set({'status': 'complete'})
+		}else{
+			this.set({'status': 'incomplete'});
+		}
+		// this.save();
+	}
 });
 
-var staffMember = new StaffMember( 
+var staffMember = new StaffMember( {}
 	// {userId: 1}
 	  // { id:1, firstName: 'Ben', lastName: 'Hadwen', entitlement:20 }
 	// urlRoot : '/user'
 	);
 
+	staffMember.on('change', function(){
+	console.log("change made");
+	// render view here
+	staffMemberView.render();
+	// this.view.render();
+	 $('#userContent').html(staffMemberView.el);
+	// this.view.render();
+
+});
+
+
+
  
 
 //VIEW
 var StaffMemberView = Backbone.View.extend({
+	tagName: 'div',
+	className: 'staffMember',
+	template: _.template('<h3>' + '<input type=checkbox '+ '<% if(status=== "complete") print("checked") %>/>' + 
+		'<%= firstName %> <%= lastName %></h3>'),
 	render: function(){
-		var html = '<p>' + this.model.get('firstName')  + " " +  this.model.get('lastName') + '</p>';
-		$(this.el).html(html);
+		var attributes = this.model.toJSON();
+		this.$el.html(this.template(attributes));
+	},
+	events:{
+		"click h3": "alertStatus",
+		'change input': "toggleStatus"
+	},
+	alertStatus: function(e){
+		// alert("h3 clicked");
+	},
+	toggleStatus: function(){
+		console.log("togglestatus-view");
+		this.model.toggleStatus();
+		this.render();
 	}
 });
 
-
- // var jqxhr = $.getJSON('/hadwensites/holidayplanner/user.json', function(data){
- 	//this simple gets the first name from JSON and puts into the model
-     //    staffMember.set({firstName: data[0].firstName});
-     // });
 
 
 
 
 var staffMemberView = new StaffMemberView({model: staffMember});
-staffMemberView.render();
-// console.log(staffMemberView.el);
+
+// staffMemberView.render();
+console.log(staffMemberView.el);
 $(document).ready(function(){
 	staffMember.fetch();
+	//output render before data from JSON fetch is returned should be default
 	// $('#userContent').html(staffMemberView.el);
 });
 
@@ -53,12 +88,13 @@ $(document).ready(function(){
 // in fact if the model is listening to changes to the modle the render should 
 // occur when the change occurs and be triggered from the modle listener
 
-  staffMember.fetch({ url: "/hadwensites/holidayplanner/js/data/usertemp.json" }).complete(function() {
+  // staffMember.fetch({ url: "/hadwensites/holidayplanner/js/data/usertemp.json" }).complete(function() {
+  staffMember.fetch().complete(function() {
   	var myInfo = staffMember.toJSON();
   	console.log("myInfo");
     console.log(myInfo);
-    staffMemberView.render();
-    $('#userContent').html(staffMemberView.el);
+    //  staffMemberView.render();
+    // $('#userContent').html(staffMemberView.el);
 
   });
 
